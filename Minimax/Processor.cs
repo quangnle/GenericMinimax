c8057216cff3c8b8cfc9a179069d9e3b680d3da8
quangnle <mnest2409@yahoo.com>
2016-01-05 18:32:51 +0800

    commit

--- Minimax/Processor.cs
+++ Minimax/Processor.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace Minimax
 {
-    public class Processor<T> where T : INode
+    public class Processor
     {
+        public IEvaluator<INode> Evaluator { get; set; }
 
-        public INode GetBestMove(T node, int depth)
+        public Processor(IEvaluator<INode> evaluator)
+        {
+            Evaluator = evaluator;
+        }
+
+        public INode GetBestMove(INode node, int depth)
         {
             if (depth == 0) return node;
 
@@ -20,7 +26,7 @@ namespace Minimax
                 var chosenNode = successors[0];
                 foreach (var successor in successors)
                 {
-                    var evalScore = Evaluate((T)successor, depth - 1, PlayerType.Min);
+                    var evalScore = Evaluator.Evaluate(successor, depth - 1, PlayerType.Min);
                     if (evalScore > maxScore)
                     {
                         maxScore = evalScore;
@@ -32,25 +38,6 @@ namespace Minimax
             }
 
             return node;
-        }
-
-        private int Evaluate(T node, int depth, PlayerType playerType)
-        {
-            var score = node.Evaluate();
-            if (depth == 0 || Math.Abs(score) >= 1000)
-                return score;
-
-            var successors = node.GetSuccessors();
-
-            if (successors != null & successors.Count > 0)
-            {
-                if (playerType == PlayerType.Max)
-                    return successors.Max(n => Evaluate((T)n, depth - 1, PlayerType.Min));
-                else
-                    return successors.Min(n => Evaluate((T)n, depth - 1, PlayerType.Max));
-            }
-
-            return score;
         }
     }
 }
